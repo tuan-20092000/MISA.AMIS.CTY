833b3f6d9410b83d9f0787327d7d04b9bf9c4f7c
tuan-20092000 <tuan0xx@gmail.com>
2021-06-17 19:33:25 +0700

    t17/6

---
+++ MISA.AMIS/MISA.Core/Interfaces/Ifarstructures/IEmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using MISA.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.Core.Interfaces.Ifarstructures
+{
+    public interface IEmployeeRepository : IBaseRepository<Employee>
+    {
+        bool CheckEmployeeCodeExist(string EmployeeCode);
+
+
+        bool CheckEmployeeCodeBeForeUpdate(Employee employee);
+
+
+        string GetMaxEmployeeCode();
+
+        //List<Employee> Search(string s);
+
+        //List<Employee> Paging(int indexStart, int count);
+    }
+}
---
+++ MISA.AMIS/MISA.Ifarstructure/Repository/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using MISA.Core.Entities;
+using MISA.Core.Interfaces.Ifarstructures;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.Ifarstructure.Repository
+{
+    public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+
+        public bool CheckEmployeeCodeBeForeUpdate(Employee employee)
+        {
+            var procedure = $"Proc_Check{ClassName}BeforeUpdate";
+            DynamicParameters dynamic = new DynamicParameters();
+            dynamic.Add("@EmployeeId", employee.EmployeeId);
+            dynamic.Add("@EmployeeCode", employee.EmployeeCode);
+            var res = DbConnection.Query<bool>(procedure, dynamic, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            return res;
+        }
+
+        public bool CheckEmployeeCodeExist(string EmployeeCode)
+        {
+            var procedure = $"Proc_Check{ClassName}CodeExist";
+            DynamicParameters dynamic = new DynamicParameters();
+            dynamic.Add($"@{ClassName}Code", EmployeeCode);
+            var res = DbConnection.Query<bool>(procedure, dynamic, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            return res;
+        }
+
+        public string GetMaxEmployeeCode()
+        {
+            var procedue = $"Proc_GetMax{ClassName}Code";
+            var res = DbConnection.Query<string>(procedue, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            return res;
+        }
+    }
+}
